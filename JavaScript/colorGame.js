59e97d14110867a54dd0c1f35c220fee365ca11f
Anthony Oyathelemhi <Tonerolima@users.noreply.github.com>
2018-04-14 05:06:51 +0100

    Update colorGame.js

--- JavaScript/colorGame.js
+++ JavaScript/colorGame.js
@@ -5,8 +5,11 @@ var header = document.querySelector("h1");
 var buttons = document.querySelectorAll("button");
 var gameMode = document.querySelectorAll(".mode")
 var reset = document.getElementById("reset");
-var highScoreText = document.getElementById("highscoretext")
-var attemptsText = document.getElementById("attemptstext")
+var highScoreText = document.getElementById("highscoretext");
+var attemptsText = document.getElementById("attemptstext");
+var alertDiv = document.getElementById("alert");
+var resultDiv = document.getElementById("header3");
+
 
 var squareArrSize = 6;
 var colors = populateColors(squareArrSize);
@@ -16,13 +19,17 @@ var gameWon = false;
 var gameOver = false;
 var rgbValue = pickColor();
 var selected = gameMode[1];
+var correctSquare = "";
 
 targetColor.textContent = rgbValue;
 
 function resetSquares(){
+	selected.style.backgroundColor = "rgb(0, 150, 200)";
+	resultDiv.style.backgroundColor = "rgb(0, 150, 200)";
 	header.style.backgroundColor = "rgb(0, 150, 200)";
 	gameWon = false;
-	// messageDisplay.textContent = "";
+	alertDiv.hidden = true;
+	alertDiv.innerHTML = '';
 	resetColors();
 }
 
@@ -30,17 +37,21 @@ for (var i = 0; i < squares.length; i++) {
 	squares[i].addEventListener("click", function(){
 		if (!gameOver) {
 			if (!gameWon) {
-				attempts++
-				attemptsText.textContent = attempts;
+				alertDiv.hidden = false;
 				if(this.style.backgroundColor == targetColor.textContent){
-					// messageDisplay.innerHTML = 'Correct! <i class="fa fa-check" aria-hidden="true"></i>';
 					highScore++
 					highScoreText.textContent = highScore;
+					alertDiv.innerHTML = '<span>Correct!</span> <i class="fa fa-check fa-lg"></i>';
+					alertDiv.style.backgroundColor = "green";
 					applySameColor();
 				} else{
-					// messageDisplay.innerHTML = 'Try Again! <i class="fa fa-times" aria-hidden="true"></i>'
 					this.style.backgroundColor = document.body.style.backgroundColor;
+					correctSquare.classList.add("correct");
+					alertDiv.innerHTML = '<span>Wrong!</span> <i class="fa fa-ban fa-lg"></i>';
+					alertDiv.style.backgroundColor = "red";
 				}
+				attempts++
+				attemptsText.textContent = attempts;
 				gameWon = true;
 			}
 
@@ -49,7 +60,7 @@ for (var i = 0; i < squares.length; i++) {
 				gameOver = true;
 			}
 			else {
-				window.setTimeout(resetSquares, 2000);
+				window.setTimeout(resetSquares, 3000);
 			}
 		}
 	})
@@ -61,12 +72,18 @@ function resetColors(){
 	targetColor.textContent = rgbValue;
 	for (var i = 0; i < squares.length; i++) {
 		squares[i].style.backgroundColor = colors[i];
+		squares[i].classList.remove("correct");
+		if (colors[i] == rgbValue) {
+			correctSquare = squares[i];
+		}
 	}
 }
 
 function resetValues(){
 	highScore = 0;
+	highScoreText.textContent = highScore;
 	attempts = 0;
+	attemptsText.textContent = attempts;
 	gameOver = false;
 	reset.textContent = "NEW COLORS"
 }
@@ -91,7 +108,9 @@ function applySameColor(){
 	for (var i = 0; i < squares.length; i++) {
 		squares[i].style.backgroundColor = rgbValue;
 	}
+	selected.style.backgroundColor = rgbValue;
 	header.style.backgroundColor = rgbValue;
+	resultDiv.style.backgroundColor = rgbValue;
 }
 
 function pickColor(){
@@ -106,10 +125,7 @@ function toggleSquares(mode){
 }
 
 reset.addEventListener("click", function(){
-	// resetColors();
 	resetSquares();
-	// populateColors();
-
 	if (gameOver === true) {
 		resetValues();
 	}	
